13ff47e658f13eb997561feee802fd18e36ab4f0
Andrej <andrej.arbanas@q.agency>
2023-09-02 15:01:15 +0200

    feat: seed users to DB; update prisma client

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,6 @@
 // prisma/seed.ts
 
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, UserType } from '@prisma/client';
 import * as dotenv from 'dotenv';
 import * as bcrypt from 'bcrypt';
 
@@ -10,7 +10,7 @@ const prisma = new PrismaClient();
 async function main() {
   dotenv.config();
   const saltOrRounds = 10;
-  const password = await bcrypt.hash(
+  const adminPassword = await bcrypt.hash(
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     process.env.ADMIN_PASSWORD!,
     saltOrRounds,
@@ -25,19 +25,41 @@ async function main() {
     },
   });
   // create admin user
-  const user = await prisma.user.upsert({
+  await prisma.user.upsert({
     where: { email: 'admin@babylon.com' },
     update: {},
     create: {
       email: 'admin@babylon.com',
-      password,
+      password: adminPassword,
       role: 'ADMIN',
       active: true,
       userAttributesId: userAttributes.id,
     },
   });
 
-  console.log(user);
+  for (let i = 0; i < 20; i++) {
+    const cities = ['Rijeka', 'Zagreb', 'Split', 'Ljubljana'];
+    const _userAttr = await prisma.userAttributes.create({
+      data: {
+        firstname: `Test_${i + 1}`,
+        lastname: `User_${i + 1}`,
+        type: Object.values(UserType)[
+          Math.floor(Math.random() * Object.values(UserType).length)
+        ],
+        city: cities[Math.floor(Math.random() * cities.length)],
+      },
+    });
+
+    await prisma.user.create({
+      data: {
+        email: `test_${i + 1}@test.com`,
+        password: Math.random().toString(36).slice(-8),
+        role: 'USER',
+        active: true,
+        userAttributesId: _userAttr.id,
+      },
+    });
+  }
 }
 
 // execute the main function
