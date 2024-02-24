40c5297494508071f201eae965820f1c8c95b293
Ivan Miljancic <ivan.miljancic@gmail.com>
2024-02-24 10:49:34 +0100

    feat: certificate-files API for managing certificate uploads

--- src/certificates/services/certificate-files.service.ts
+++ src/certificates/services/certificate-files.service.ts
@@ -1,3 +1,6 @@
+import { randomUUID } from 'node:crypto';
+import { extname } from 'node:path';
+
 import { Injectable } from '@nestjs/common';
 
 import {
@@ -5,18 +8,39 @@ import {
   CertificateUploadUrlResponseDto,
 } from '../dto';
 import { S3Service } from '../../storage/s3.service';
+import { CERTIFICATE_STORAGE_KEY } from '../constants';
+import { resolveContentType } from '../../common/helpers';
 
 @Injectable()
 export class CertificateFilesService {
   constructor(private readonly s3Service: S3Service) {}
 
-  createUploadURL(
+  async createUploadURL(
     data: CreateCertificateUploadURLDto,
   ): Promise<CertificateUploadUrlResponseDto> {
-    throw new Error('Method not implemented.');
+    const fileExtension = extname(data.filename);
+    const certificateKey = this.generateCertificateKey(fileExtension);
+    const contentType = resolveContentType(fileExtension);
+
+    const uploadUrl = await this.s3Service.getPresignedUploadUrl({
+      key: certificateKey,
+      contentType,
+    });
+
+    return {
+      key: certificateKey,
+      contentType,
+      uploadUrl,
+    };
   }
 
-  deleteCertificateFromStorage(certificateKey: string): Promise<void> {
-    throw new Error('Method not implemented.');
+  async deleteCertificateFromStorage(certificateKey: string): Promise<void> {
+    await this.s3Service.deleteOne(certificateKey);
+  }
+
+  private generateCertificateKey(fileExtension: string): string {
+    const uuid = randomUUID();
+
+    return `${CERTIFICATE_STORAGE_KEY}/${uuid}${fileExtension}`;
   }
 }
