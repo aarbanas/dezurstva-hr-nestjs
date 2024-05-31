f068391a22e472958f2cbf83526ffa5fb93e0f01
Andrej Arbanas <andrej.arbanas@porsche.digital>
2024-05-31 14:53:30 +0200

    feat: subscriptions model WIP

---
+++ prisma/migrations/20240524073853_subscriptions/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Subscription" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "activeTill" TIMESTAMP(3),
+    "userId" INTEGER,
+
+    CONSTRAINT "Subscription_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Subscription" ADD CONSTRAINT "Subscription_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
