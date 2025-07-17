126615a78c3e0d39185ee2043339585bc5d65cf7
Andrej Arbanas <andrej.arbanas@porsche.digital>
2025-07-17 16:40:03 +0200

    feat: add discord webhook

--- src/redis/redis.controller.ts
+++ src/redis/redis.controller.ts
@@ -2,6 +2,7 @@ import { Controller, ForbiddenException, Get, Headers } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { EmailQueueService } from './email/email-queue.service';
 import { EmailCacheService } from './email/email-cache.service';
+import axios from 'axios';
 
 @Controller('redis')
 export class RedisController {
@@ -18,9 +19,27 @@ export class RedisController {
   async processEmailQueue(@Headers('authorization') token: string) {
     if (token !== this.token) throw new ForbiddenException();
 
+    await this.sendDiscordMessage(
+      '🛠️ Cron job executed: Email queue processing.',
+    );
+
     await this.emailCacheService.resetDailyEmailCount();
     await this.emailQueueService.processQueue();
 
+    await this.sendDiscordMessage(
+      '✅ Cron job finished: Queue processing completed.',
+    );
+
     return { success: true };
+  }
+
+  private async sendDiscordMessage(content: string): Promise<void> {
+    const discordRedriveQueueWebhookUrl = this.configService.getOrThrow(
+      'DISCORD_EMPTY_QUEUE_WEBHOOK',
+    );
+
+    await axios.post(discordRedriveQueueWebhookUrl, {
+      content,
+    });
   }
 }
